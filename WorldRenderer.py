a3f1b5e193482ba206c53d06985ce6ce69ee81a4
Fred <guru.three@gmail.com>
2019-11-23 23:48:59 +0000

    renderer test code

--- WorldRenderer.py
+++ WorldRenderer.py
@@ -3,11 +3,20 @@ import pygame
 
 class WorldRenderer:
     """Render the world so we can see what the robot is doing!"""
-    TheWorld = None
 
     def __init__(self, TheWorld):
         self.TheWorld = TheWorld
 
+        pygame.init()
+        pygame.display.set_caption("piwarsimulator")
+
+        self.screen = pygame.display.set_mode((800,600))
+
+    def update(self):
+        pygame.display.flip()
+
+#    def __del__(self):
+        
 
     def __str__(self):
         return repr(self.TheWorld)
--- piwarsimulator.py
+++ piwarsimulator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from WorldObject import *
+from WorldRenderer import *
 
 TheWorld = []
 
@@ -9,5 +10,15 @@ TheWorld.append(robot)
 TheWorld.append(WorldObject(objecttype=ObjectType.TARGET, x=1, y=0))
 TheWorld.append(WorldObject(objecttype=ObjectType.ZONE, x=2, y=0))
 
-print(TheWorld)
+#print(TheWorld)
 
+renderer = WorldRenderer(TheWorld)
+
+running = True
+while running:
+    TheWorld[0].x += 0.01
+    renderer.update()
+    if TheWorld[0].x >= 1:
+        running = False
+
+#print(renderer)
