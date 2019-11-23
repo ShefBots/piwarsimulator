113b0314a7455531f99d9b98b11aba895c60dd01
Fred <guru.three@gmail.com>
2019-11-23 23:31:47 +0000

    default testing world for barrels!

--- piwarsimulator.py
+++ piwarsimulator.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 from WorldObject import *
 
-robot = WorldObject(radius=1,objecttype=ObjectType.ROBOT)
-print(robot)
-print(repr(robot))
+TheWorld = []
+
+robot = WorldObject(objecttype=ObjectType.ROBOT, radius=0.1)
+
+TheWorld.append(robot)
+TheWorld.append(WorldObject(objecttype=ObjectType.TARGET, x=1, y=0))
+TheWorld.append(WorldObject(objecttype=ObjectType.ZONE, x=2, y=0))
+
+print(TheWorld)
+
