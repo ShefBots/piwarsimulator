36ae75677986bab221dddc1d38f9fd8554e544d2
Fred <guru.three@gmail.com>
2019-11-23 23:27:11 +0000

    change defauld radius

--- WorldObject.py
+++ WorldObject.py
@@ -6,14 +6,14 @@ class WorldObject:
     x = 0
     y = 0
     angle = 0
-    radius = 0
+    radius = 0.01
     objecttype = ObjectType.UNKNOWN
 
     def __init__(self, *args, **kwargs):
         self.x = kwargs.get('x', 0)
         self.y = kwargs.get('y', 0)
         self.angle = kwargs.get('angle', 0)
-        self.radius = kwargs.get('radius', 0)
+        self.radius = kwargs.get('radius', 0.01)
         self.objecttype = kwargs.get('objecttype', 0)
 
     def __str__(self):
