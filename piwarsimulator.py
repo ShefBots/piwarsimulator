1195b3aaa11337b0d30b23289c5ed00d69301cad
Fred <guru.three@gmail.com>
2019-11-30 17:38:15 +0000

    create a random barrel layout

--- piwarsimulator.py
+++ piwarsimulator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+import random
 from WorldObject import *
 from WorldRenderer import *
 from ScanObject import *
@@ -8,14 +9,26 @@ from RobotBrain import *
 
 TheWorld = []
 
-robot = WorldObject(objecttype=ObjectType.ROBOT, radius=0.1)
-robotbrain = RobotBrain(robot=robot, speed=0.01, turningspeed=3)
+robot = WorldObject(objecttype=ObjectType.ROBOT, x=1, radius=0.1)
+robotbrain = RobotBrain(robot=robot, speed=0.02, turningspeed=3)
 
 # the order this is constructed in is the rendering order...
 TheWorld.append(robot) # this should always be index 0!
-TheWorld.append(WorldObject(objecttype=ObjectType.ZONE, x=-1, y=0.5, radius=0.1, color='blue'))
-TheWorld.append(WorldObject(objecttype=ObjectType.TARGET, x=1, y=0.2, radius=0.056, color='blue'))
-TheWorld.append(WorldObject(objecttype=ObjectType.TARGET, x=0, y=-0.5, radius=0.056, color='blue'))
+TheWorld.append(WorldObject(objecttype=ObjectType.ZONE, x=-1.25, y=0.5, radius=0.1, color='blue'))
+TheWorld.append(WorldObject(objecttype=ObjectType.ZONE, x=-1.25, y=-0.5, radius=0.1, color='darkgreen'))
+
+# check zone dimensions in rules, then maximum possible number of barrels that would fit
+
+#TheWorld.append(WorldObject(objecttype=ObjectType.TARGET, x=1.25, y=0.2, radius=0.056, color='blue'))
+#TheWorld.append(WorldObject(objecttype=ObjectType.TARGET, x=0, y=-0.5, radius=0.056, color='blue'))
+
+for i in range(0, random.randint(15, 40)):
+    if random.random() < 0.5:
+        c = 'blue'
+    else:
+        c = 'darkgreen'
+    TheWorld.append(WorldObject(objecttype=ObjectType.TARGET, \
+        x=random.random()*2-1, y=random.random()*2-1, radius=0.056, color=c))
 
 renderer = WorldRenderer(TheWorld)
 
