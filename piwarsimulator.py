ced53d3f4c5fbeadbb702c218af39ca3d9362651
Fred <guru.three@gmail.com>
2023-12-27 12:34:09 +0000

    reorganisation and world rendering option

--- piwarsimulator.py
+++ piwarsimulator.py
@@ -7,93 +7,99 @@ from os import listdir
 from signal import signal, SIGINT
 from sys import exit
 
-# list of available brains for argument list
-brains = sorted([s[:-3] for s in listdir('brains/') if "Brain.py" in s])
-maps = sorted([s[:-3] for s in listdir('world/') if "Map.py" in s])
 
-parser = argparse.ArgumentParser(description="Simulator for ShefBots Mark 1b for PiWars 2024")
-parser.add_argument("--brain", help="robot brain/challenge (default RobotBrain)", default="RobotBrain", choices=brains)
-parser.add_argument("--map", help=f"map (default {maps[0]})", default=maps[0], choices=maps)
-parser.add_argument("--mode", help="operation mode (default simulation)", default="simulation", choices=['simulation', 'reality'])
-parser.add_argument("--rendering", help="render world on screen (default true)", default='true', choices=['true', 'false'])
-args = parser.parse_args()
-
-
-# from brains.RobotBrain import RobotBrain
-# from brains.EcoDisasterBrain import EcoDisasterBrain
-
-from controllers.SimulatedMovementController import SimulatedMovementController
-from sensors.SimulatedVision360 import SimulatedVision360
-
-from world.WorldObject import *
-from world.WorldRenderer import *
-
-# TODO add log class, use that for output instead of print
-# TODO for now everything is simulated
-# TODO proper dimensions and whatnot
-
-# TODO option to select simulated/real
-# TODO option for rendering
+# TODO proper dimensions/sizes of items
+# TODO control mode for real hardware
+# TODO sensor_simulation for real hardware but simulated sensor input
 # TODO side by side output of world and sensor
 
-running = True
+running = True # state of simulator
+frame_time = 1/60.0 # aim for 60 fps simulation/processing
 
-def handler(signal_received, frame):
+def sigint_handler(signal_received, frame):
     """trap/handle ctrl c"""
     global running # why does this need to be global?
     print('SIGINT or CTRL-C detected. Exiting gracefully')
     running = False
-signal(SIGINT, handler)
+    time.sleep(0.1) # wait a moment for pygame
+    exit()
+signal(SIGINT, sigint_handler)
 
+# list of available brains for argument list
+brains = sorted([s[:-3] for s in listdir('brains/') if "Brain.py" in s])
+maps = sorted([s[:-3] for s in listdir('world/') if "Map.py" in s])
 
-ExteriorTheWorld = []
+parser = argparse.ArgumentParser(description="Simulator/controller for ShefBots Mark 1b for PiWars 2024")
+parser.add_argument("--brain", help="robot brain/challenge (default RobotBrain)", default="RobotBrain", choices=brains)
+parser.add_argument("--map", help=f"map (default {maps[0]})", default=maps[0], choices=maps)
+parser.add_argument("--mode", help="operation mode (default simulation)", default="simulation", choices=['simulation', 'sensor_simulation', 'control'])
+parser.add_argument("--rendering", help="render world on screen (default true)", default='true', choices=['true', 'false'])
+args = parser.parse_args()
 
-robot = WorldObject(object_type=ObjectType.ROBOT, x=0, y=0, radius=0.1, angle=0) # units metres and degress
+if args.mode == 'simulation':
+    from world.WorldObject import *
+    from controllers.SimulatedMovementController import SimulatedMovementController
+    from sensors.SimulatedVision360 import SimulatedVision360
+elif args.mode == 'sensor_simulation':
+    # TODO real hardware, simulated sensors
+    pass
+elif args.mode == 'control':
+    # TODO real hardware
+    pass
 
 # the order this is constructed in is also the rendering order...
+ExteriorTheWorld = []
+robot = WorldObject(object_type=ObjectType.ROBOT, x=0, y=0, radius=0.1, angle=0) # units metres and degress
 ExteriorTheWorld.append(robot) # this should always be index 0!
 
 # import the map
-print(f"Loading map {args.map}...")
-map = getattr(importlib.import_module('world.'+args.map), args.map)
-map(ExteriorTheWorld)
+if args.mode == 'simulation' or args.mode == 'sensor_simulation':
+    print(f"Loading map {args.map}...")
+    map = getattr(importlib.import_module('world.'+args.map), args.map)
+    map(ExteriorTheWorld)
 
 # logic for the robot
 print("Loading robot controller...")
 if args.mode == 'simulation':
-    sim_controller = SimulatedMovementController(robot)
+    controller = SimulatedMovementController(robot)
 else:
     raise Exception("No real hardware controller yet")
-# sim_controller.set_plane_velocity([0, 0.05])
-# sim_controller.set_angular_velocity(5)
-# robot_brain = RobotBrain(robot=robot, controller=sim_controller, speed=0.3, turning_speed=45)
-# robot_brain = EcoDisasterBrain(robot=robot, controller=sim_controller, speed=0.05, turning_speed=10)
 
 print(f"Loading {args.brain}...")
 brain = getattr(importlib.import_module('brains.'+args.brain), args.brain)
-robot_brain = brain(robot=robot, controller=sim_controller, speed=0.05, turning_speed=10)
-robot_brain.add_sensor(SimulatedVision360(ExteriorTheWorld))
+robot_brain = brain(robot=robot, controller=controller, speed=0.05, turning_speed=10)
 
-renderer = WorldRenderer(x_res=1000, y_res=1000, world_scale=400) # default 0,0 is centre of screen
-renderer.update()
-print("Getting ready...")
-time.sleep(1) # wait for the world to load
+print("Attaching sensors...")
+if args.mode == 'simulation' or args.mode == 'sensor_simulation':
+    robot_brain.add_sensor(SimulatedVision360(ExteriorTheWorld))
+else:
+    # TODO real hardware
+    pass
+
+if args.rendering == 'true':
+    from world.WorldRenderer import *
+    renderer = WorldRenderer(x_res=1000, y_res=1000, world_scale=400) # default 0,0 is centre of screen
+    renderer.update()
+
+print("Waiting...")
+time.sleep(1) # wait for things to settle
 
 print("Running...")
-frame_time = 1/60.0 # aim for 60 fps simulation
 while running:
     now = time.time();
 
     robot_brain.process()
     #robot_brain.simulate(dt)
 
-    renderer.update(ExteriorTheWorld)
-    #renderer.update(ExteriorTheWorld, robot_brain.TheWorld) # see the world as it is and as the robot sees it
-    #renderer.update(robot_brain.TheWorld) # see the world as the robot sees it
-    if renderer.running == False:
-        running = False
+    if args.rendering == 'true':
+        renderer.update(ExteriorTheWorld)
+        #renderer.update(ExteriorTheWorld, robot_brain.TheWorld) # see the world as it is and as the robot sees it
+        #renderer.update(robot_brain.TheWorld) # see the world as the robot sees it
+        if renderer.running == False:
+            running = False
+    
     to_sleep = frame_time - (time.time() - now)
     if to_sleep > 0:
         time.sleep(to_sleep)
 
-sim_controller.stop(exiting=True)
+controller.stop(exiting=True)
