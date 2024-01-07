cdabe37b8500e34296e7fe234d9bf665a8e38ba5
Fred <guru.three@gmail.com>
2024-01-07 10:22:16 +0000

    comments, fix stoping

--- brains/MazeBrain.py
+++ brains/MazeBrain.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 from time import time
-
 from world.WorldObject import *
 from world.ObjectType import *
 from brains.RobotBrain import RobotBrain
 
 
 class MazeBrain(RobotBrain):
-    """logic for the escape route following challenge"""
+    """logic for the escape route challenge"""
 
     SENSOR_HEADINGS = [-90, 0, 90]
     # how close to get to walls before moving to next program stage
@@ -20,14 +19,14 @@ class MazeBrain(RobotBrain):
     MOVE_RIGHT = 3
     STOPPED = 4
 
-    CURRENT_MODE = 0
+    CURRENT_MODE = ALIGNMENT
 
     def __init__(self, **kwargs):
         super(MazeBrain, self).__init__(**kwargs)
         self.last_reading = time()
 
     def process(self):
-        """do the basic brain stuff then do specific ecodisaster things"""
+        """do the basic brain stuff then do specific escape route things"""
 
         # check sensors and stop if collision is imminent
         super().process()
@@ -35,19 +34,23 @@ class MazeBrain(RobotBrain):
         if self.sensor_measurements["manual_control"]:
             return
 
+        # do nothing if halted
         if self.CURRENT_MODE == self.STOPPED:
             return
 
+        # make sure we're parallel to a wall
         if self.CURRENT_MODE == self.ALIGNMENT:
             # TODO wall alignment routine
-            # move side to side checking forward sensor distance change
+            # move side to side checking forward and side sensors distance change
             # use angle of that to rotate a specified amount
+            # then move backwards some to ensure distance to walls > WALL_STOP_DISTANCE
             self.CURRENT_MODE = self.MOVE_LEFT
             self.controller.set_plane_velocity([-self.speed, 0])
 
         # get the distances to walls in each heading
         distances = self.find_distances()
 
+        # move in directions with space after getting close-ish to a wall
         if self.CURRENT_MODE == self.MOVE_LEFT:
             if not distances[0] == None and distances[0] < self.WALL_STOP_DISTANCE:
                 self.controller.set_plane_velocity([0, self.speed])
@@ -63,20 +66,20 @@ class MazeBrain(RobotBrain):
                     # there's space on the left, go that way
                     self.controller.set_plane_velocity([-self.speed, 0])
                     self.CURRENT_MODE = self.MOVE_LEFT
+                else:
+                    # possibly not square anymore? reenter alignment
+                    self.controller.stop()
+                    self.CURRENT_MODE == self.ALIGNMENT
 
             if time() - self.last_reading > (0.03 / self.speed):
                 # stop if no walls have been seen for X seconds (30 cm distance equiv)
                 self.controller.stop()
-                self.current_MODE = self.STOPPED
+                self.CURRENT_MODE = self.STOPPED
 
         elif self.CURRENT_MODE == self.MOVE_RIGHT:
             if not distances[2] == None and distances[2] < self.WALL_STOP_DISTANCE:
                 self.controller.set_plane_velocity([0, self.speed])
                 self.CURRENT_MODE = self.MOVE_FORWARD
-
-        # which ever of the three time of flight sensors has the furthest reading, head towards that
-        # or move so that ideally each time of flight reports 5 cm
-        # do forward sensor, then left sensor, then right sensor
 
     def find_distances(self):
         """find the walls in each direction"""
