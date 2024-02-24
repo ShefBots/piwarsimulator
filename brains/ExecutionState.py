6dfe5c0300f6266f8868a9c42972a77b36ce250b
Fred <guru.three@gmail.com>
2024-02-24 12:40:15 +0000

    new complete execution state

--- brains/ExecutionState.py
+++ brains/ExecutionState.py
@@ -10,6 +10,7 @@ class ExecutionState(Enum):
     SQUARING_UP = 2  # aligning to wall
     PROGRAM_INIT = 3  # something to take of before control
     PROGRAM_CONTROL = 4  # do something smart
+    PROGRAM_COMPLETE = 5  # done
 
     # states for other brains
     MOVE_LEFT = 10
@@ -27,8 +28,13 @@ class ExecutionState(Enum):
             self.STOPPED: "Stopped",
             self.NO_CONTROL: "No Control",
             self.SQUARING_UP: "Squaring Up",
+            self.PROGRAM_INIT: "Program Initialisation",
             self.PROGRAM_CONTROL: "Program Control",
+            self.PROGRAM_COMPLETE: "Program Complete",
             self.MOVE_LEFT: "Moving Left",
             self.MOVE_FORWARD: "Moving Forward",
             self.MOVE_RIGHT: "Moving Right",
+            self.MOVE_TO_BARREL: "Moving to Barrel",
+            self.MOVE_TO_ZONE: "Moving to Zone",
+            self.DROP_OFF_BARREL: "Dropping off Barrel",
         }[self]
