78a762fde883243532487ca90f81a6ad4df12f17
Fred <guru.three@gmail.com>
2024-04-13 19:08:17 +0100

    control scheme for launcher

--- sensors/RadioControl.py
+++ sensors/RadioControl.py
@@ -18,8 +18,12 @@ class RadioControl(Sensor):
     FORWARD_CHANNEL = 2
     RIGHT_CHANNEL = 3
     TURN_CHANNEL = 0
-    SPEED_CHANNEL = 5
-    EN_CHANNEL = 4
+
+    EN_CHANNEL = 7  # Right most switch: enable manual control
+
+    BRUSHLESS_CHANNEL = 5  # Left Pot: Brushless motor speed
+    TILT_CHANNEL = 6  # Right Pot: Tilt the turret up and down
+    FIRE_CHANNEL = 4  # Left most switch: fire/advance
 
     LIN_SCALE = 1.0
     ANG_SCALE = 180.0
@@ -40,12 +44,18 @@ class RadioControl(Sensor):
         self.remote.assign_channel_decoder(self.RIGHT_CHANNEL, analog_decoder)
         # Angular
         self.remote.assign_channel_decoder(self.TURN_CHANNEL, analog_decoder)
-        # Enable channel
+
+        # Enable manual control
         self.remote.assign_channel_decoder(self.EN_CHANNEL, binary_decoder)
-        # Speed
+
+        # Launcher brushless
         self.remote.assign_channel_decoder(
-            self.SPEED_CHANNEL, analog_biased_decoder
+            self.BRUSHLESS_CHANNEL, analog_biased_decoder
         )
+        # Launcher tilt
+        self.remote.assign_channel_decoder(self.TILT_CHANNEL, analog_biased_decoder)
+        # Launcher fire/advance
+        self.remote.assign_channel_decoder(self.FIRE_CHANNEL, binary_decoder)
 
         print("Establishing Connection...")
         while not self.remote.is_connected():
@@ -59,34 +69,31 @@ class RadioControl(Sensor):
         manual_control = False
 
         if self.remote.is_connected() and self.remote.check_receive():
-            manual_control = not self.remote.read_channel(self.EN_CHANNEL)
+            manual_control = self.remote.read_channel(self.EN_CHANNEL)
 
             if manual_control:
-                speed_scale = self.remote.read_channel(self.SPEED_CHANNEL)
-
-                # note all speeds here are scaled according to the max speed
-                # control and then the max speed (& turning speed) of robot
+                # read channel returns -1 to 1, scale by robot speed for velocity
                 forward_vel = (
-                    self.remote.read_channel(self.FORWARD_CHANNEL)
-                    * self.speed
-                    * speed_scale
+                    self.remote.read_channel(self.FORWARD_CHANNEL) * self.speed
                 )
-                sideways_vel = (
-                    self.remote.read_channel(self.RIGHT_CHANNEL)
-                    * self.speed
-                    * speed_scale
-                )
+                sideways_vel = self.remote.read_channel(self.RIGHT_CHANNEL) * self.speed
                 angular_vel = (
-                    self.remote.read_channel(self.TURN_CHANNEL)
-                    * self.turning_speed
-                    * speed_scale
+                    self.remote.read_channel(self.TURN_CHANNEL) * self.turning_speed
                 )
+
+                # for launcher control
+                brushless_speed = self.remote.read_channel(self.BRUSHLESS_CHANNEL)
+                tilt = self.remote.read_channel(self.TILT_CHANNEL)
+                fire = self.remote.read_channel(self.FIRE_CHANNEL)
 
         inputs = {
             "forward_vel": forward_vel,
             "sideways_vel": sideways_vel,
             "angular_vel": angular_vel,
             "manual_control": manual_control,
+            "brushless_speed": brushless_speed,
+            "tilt": tilt,
+            "fire": fire,
         }
 
         return [], inputs
