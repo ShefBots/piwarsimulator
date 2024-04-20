a4a817fb773fa355c6455308cf9c4b3b1efc90fc
Fred <guru.three@gmail.com>
2024-04-20 19:41:18 +0100

    script for zombie apoc lol

---
+++ challenge_scripts/zombie_apocalypse.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# The name of this challenge
+CHALLENGENAME="Zombie Apocalypse"
+
+# The basic command for this challenge
+PYARGS="piwarsimulator.py --brain RobotBrain --attachment launcher --beam false --vision_mode none --robot_speed 0.2 --turning_speed 45 --enable_safeties false"
+
+# Get the directory containing the script
+SCRIPT_DIR=$(dirname "$0")
+
+# Load shared functions
+cd "$SCRIPT_DIR"
+source shared.sh
+
+# Change to the parent directory of the script
+cd ..
+
+# Check the first argument
+if [ "$1" = "ctrl" ]; then
+    ctrl_shared
+elif [ "$1" = "sim" ] || [ $# -eq 0 ]; then
+    PYARGS+=" --map EmptyMap"
+    sim_shared
+elif [ "$1" = "-h" ]; then
+    # Display usage information
+    echo $USAGE
+    exit
+else
+    echo "Invalid argument: $1"
+    echo $USAGE
+    exit 1
+fi
+
+echo "Launching $CHALLENGENAME..."
+echo python3 $PYARGS "${@:2}"
+python3 $PYARGS "${@:2}"
